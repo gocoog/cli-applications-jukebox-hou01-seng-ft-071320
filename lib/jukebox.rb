bc78c5ad9971a1d7045f18a8ae49687defb007fc
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-12 17:34:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -56,9 +56,10 @@ def exit_jukebox
 end
 
 def run(songs)
-  puts "Please enter a command:"
-  input = gets.strip
+  
   unless input == "exit"
+    puts "Please enter a command:"
+    input = gets.strip
     if input == "list"
       list(songs)
     elsif input == play 
