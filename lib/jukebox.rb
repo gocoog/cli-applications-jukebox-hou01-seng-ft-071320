18dc69276d6f9b888db8bed3c5d1ba80dfe9bfc5
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-12 17:23:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,7 +42,7 @@ def play(songs)
     index += 1
     if input == index.to_s
       puts "Playing #{name}"
-    elsif name == input
+    elsif i[name] == input
       puts "Playing #{name}"
     else
       puts "Invalid input, please try again"
