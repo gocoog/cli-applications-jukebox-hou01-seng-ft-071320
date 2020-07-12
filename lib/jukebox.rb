35bc3b963dee9a8d46f647e4ce074f5c6e1cec15
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-12 17:44:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -38,8 +38,16 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.chomp
   
-  songs.each do |name|
-    binding.pry
+  songs.each_with_index do |name, index|
+    index += 1
+    if input == index.to_s
+      puts "Playing #{name}"
+    elsif name == input
+      puts "Playing #{name}"
+    end
+    if input != index.to_s || input != name
+      puts "Invalid input, please try again"
+    end
   end
   
 end
